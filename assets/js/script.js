569fae91ab8af209ac1f88032881798bd2091228
EwanColquhoun <ewancolquhoun@hotmail.com>
2021-07-28 08:36:14 +0000

    Add Division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function() {
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
     } else if (gameType === "division") {
-        displayMultiplyQuestion(num1, num2);
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -85,7 +85,7 @@ document.addEventListener("DOMContentLoaded", function() {
           return [operand1 * operand2, "multiply"];
       } else if (operator === "-") {
           return [operand1 - operand2, "subtract"];
-      } else if (operator ==="/") {
+      } else if (operator === "/") {
           return [operand1 / operand2, "division"];      
       } else {
           alert(`Unimplemented operator ${operator}`);
@@ -135,7 +135,7 @@ document.addEventListener("DOMContentLoaded", function() {
   
   function displayDivisionQuestion(operand1, operand2) {
   
-        document.getElementById('operand1').textContent = operand1;
-        document.getElementById('operand2').textContent = operand2;
+        document.getElementById('operand1').textContent = operand1 * operand2
+        document.getElementById('operand2').textContent = operand2
         document.getElementById('operator').textContent = "/";
   }
